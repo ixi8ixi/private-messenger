a890c22917afe40a77bd133305014c25d7776674
Belocerkovchenko Andrey <ix8oio8xi@gmail.com>
2024-04-06 21:23:55 +0300

    Desktop-client: remove some comments

--- client/desktop/src/main/java/com/earuile/bubble/ClientApplication.java
+++ client/desktop/src/main/java/com/earuile/bubble/ClientApplication.java
@@ -16,15 +16,6 @@ public class ClientApplication {
 
     @Bean
     public FxWeaver fxWeaver(ConfigurableApplicationContext applicationContext) {
-        // Would also work with javafx-weaver-core only:
-        // return new FxWeaver(applicationContext::getBean, applicationContext::close);
         return new SpringFxWeaver(applicationContext);
     }
-
-//    @Bean
-//    public FxWeaver fxWeaver(ConfigurableApplicationContext applicationContext) {
-//        // Would also work with javafx-weaver-core only:
-//        // return new FxWeaver(applicationContext::getBean, applicationContext::close);
-//        return new SpringFxWeaver(applicationContext);
-//    }
 }
